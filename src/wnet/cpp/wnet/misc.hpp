c17fc7dd1afbc00fab584778087fed34213bc6da
Michał Startek <michal.startek@mimuw.edu.pl>
2025-08-25 23:50:54 +0200

    Fix return type for flows

--- src/wnet/cpp/wnet/misc.hpp
+++ src/wnet/cpp/wnet/misc.hpp
@@ -1,6 +1,15 @@
 #ifndef WNET_MISC_HPP
 #define WNET_MISC_HPP
 
+#include <string>
+#include <vector>
+#include <cstring> // for std::memcpy
+
+#include <nanobind/nanobind.h>
+#include <nanobind/stl/string.h>
+#include <nanobind/stl/vector.h>
+#include <nanobind/ndarray.h>
+namespace nb = nanobind;
 
 inline std::string get_type_str(const nb::object &obj) {
     nb::handle type = obj.type();
@@ -9,5 +18,17 @@ inline std::string get_type_str(const nb::object &obj) {
     return std::string(module.c_str()) + "." + std::string(name.c_str());
 }
 
+template<typename T>
+nb::ndarray<nb::numpy, T, nb::ndim<1>> vector_to_numpy(const std::vector<T>& vec) {
+    // Create a 1D NumPy array from the vector
+    T* data = new T[vec.size()];
+    std::memcpy(data, vec.data(), vec.size() * sizeof(T));
+    nb::capsule owner(data, [](void* p) noexcept { delete[] static_cast<T*>(p); });
+    return nb::ndarray<nb::numpy, T, nb::ndim<1>>(
+        data,
+        { vec.size() },
+        owner
+    );
+}
 
 #endif // WNET_MISC_HPP
--- src/wnet/cpp/wnet/wnet.cpp
+++ src/wnet/cpp/wnet/wnet.cpp
@@ -3,19 +3,24 @@
 #include <nanobind/nanobind.h>
 #include <nanobind/stl/string.h>
 #include <nanobind/stl/vector.h>
+#include <nanobind/stl/tuple.h>
+#include <nanobind/ndarray.h>
 
 #include "decompositable_graph.hpp"
 #include "graph_elements.hpp"
+#include "distribution.hpp"
+#include "misc.hpp"
 
 
 NB_MODULE(wnet_cpp, m) {
     m.doc() = "WNet C++ imlementation module";
-
+    m.def("one_arr", []() { std::vector<int> v = {1,2,3}; return std::make_tuple(vector_to_numpy(v)); });
     // Define a simple function to demonstrate the module
     m.def("wnet_cpp_hello", []() {
         std::cout << "Hello from WNet (C++)!" << std::endl;
     }, "A simple hello world function for the WNet (C++) extension");
     // Bind the classes to the module
+
     nb::class_<FlowNode>(m, "FlowNode")
         .def(nb::init<LEMON_INT, SourceNode>())
         .def(nb::init<LEMON_INT, SinkNode>())
@@ -26,6 +31,7 @@ NB_MODULE(wnet_cpp, m) {
         .def("layer", &FlowNode::layer)
         .def("type_str", &FlowNode::type_str)
         .def("to_string", &FlowNode::to_string);
+
     nb::class_<FlowEdge>(m, "FlowEdge")
         .def(nb::init<LEMON_INT, const FlowNode&, const FlowNode&, FlowEdgeType>())
         .def("get_id", &FlowEdge::get_id)
@@ -35,6 +41,7 @@ NB_MODULE(wnet_cpp, m) {
         .def("get_end_node_id", &FlowEdge::get_end_node_id)
         .def("get_type", &FlowEdge::get_type)
         .def("to_string", &FlowEdge::to_string);
+
     nb::class_<WassersteinNetworkSubgraph>(m, "WassersteinNetworkSubgraph")
         .def(nb::init<const std::vector<size_t>&, const std::vector<FlowNode>&, const std::vector<FlowEdge>&, size_t>())
         .def("add_simple_trash", &WassersteinNetworkSubgraph::add_simple_trash)
@@ -47,6 +54,7 @@ NB_MODULE(wnet_cpp, m) {
         .def("no_edges", &WassersteinNetworkSubgraph::no_edges)
         .def("get_nodes", &WassersteinNetworkSubgraph::get_nodes)
         .def("get_edges", &WassersteinNetworkSubgraph::get_edges);
+
     nb::class_<WassersteinNetwork>(m, "WassersteinNetwork")
         .def(nb::init<const Distribution*, const std::vector<Distribution*>&, const nb::callable*, LEMON_INT>())
         .def("add_simple_trash", &WassersteinNetwork::add_simple_trash)
@@ -55,9 +63,25 @@ NB_MODULE(wnet_cpp, m) {
         .def("total_cost", &WassersteinNetwork::total_cost)
         .def("no_subgraphs", &WassersteinNetwork::no_subgraphs)
         .def("get_subgraph", &WassersteinNetwork::get_subgraph, nb::rv_policy::reference)
-        .def("to_string", &WassersteinNetwork::to_string)
+        .def("__str__", &WassersteinNetwork::to_string)
         .def("lemon_to_string", &WassersteinNetwork::lemon_to_string)
-        .def("flows_for_spectrum", &WassersteinNetwork::flows_for_spectrum);
+        .def("no_subgraphs", &WassersteinNetwork::no_subgraphs)
+        .def("get_subgraph", &WassersteinNetwork::get_subgraph, nb::rv_policy::reference)
+        .def("lemon_to_string", &WassersteinNetwork::lemon_to_string)
+        .def("flows_for_spectrum", [](WassersteinNetwork& self, size_t spectrum_id) {
+            auto [empirical_peak_indices, theoretical_peak_indices, flows] = self.flows_for_spectrum(spectrum_id);
+            return std::make_tuple(vector_to_numpy<size_t>(empirical_peak_indices),
+                                   vector_to_numpy<size_t>(theoretical_peak_indices),
+                                   vector_to_numpy<LEMON_INT>(flows));
+        }, nb::rv_policy::move)
+        .def("count_empirical_nodes", &WassersteinNetwork::count_nodes_of_type<EmpiricalNode>)
+        .def("count_theoretical_nodes", &WassersteinNetwork::count_nodes_of_type<TheoreticalNode>)
+        .def("count_matching_edges", &WassersteinNetwork::count_edges_of_type<MatchingEdge>)
+        .def("count_theoretical_to_sink_edges", &WassersteinNetwork::count_edges_of_type<TheoreticalToSinkEdge>)
+        .def("count_src_to_empirical_edges", &WassersteinNetwork::count_edges_of_type<SrcToEmpiricalEdge>)
+        .def("count_simple_trash_edges", &WassersteinNetwork::count_edges_of_type<SimpleTrashEdge>)
+        .def("matching_density", &WassersteinNetwork::matching_density);
+
     nb::class_<Distribution>(m, "Distribution")
         .def(nb::init<nb::ndarray<>, nb::ndarray<LEMON_INT, nb::shape<-1>>>())
         .def("size", &Distribution::size)
@@ -65,6 +89,7 @@ NB_MODULE(wnet_cpp, m) {
         .def("get_intensities", &Distribution::get_intensities)
         .def("get_point", &Distribution::get_point)
         .def("closer_than", &Distribution::closer_than);
+
     nb::class_<Distribution::Point_t>(m, "DistributionPoint")
         .def_ro("positions", &Distribution::Point_t::first)
         .def_ro("index", &Distribution::Point_t::second);
