83ae6234b3546142d11c4f4833a2c9d68dc97ba2
Michał Startek <michal.startek@mimuw.edu.pl>
2025-09-12 14:04:28 +0200

    Better example

--- example.py
+++ example.py
@@ -2,12 +2,20 @@ from wnet import Distribution, WassersteinNetwork
 from wnet.distances import L1Distance, wrap_distance_function
 import numpy as np
 
-S1 = Distribution(np.array([[0], [0]]), np.array([1]))
-S2 = Distribution(np.array([[1], [0]]), np.array([1]))
+positions1 = np.array([[0, 1, 5, 15], [0, 0, 0, 0]])
+masses1 = np.array([10, 5, 5, 5])
 
-W = WassersteinNetwork(S1, [S2], L1Distance(), 10)
+positions2 = np.array([[1,10], [0, 0]])
+masses2 = np.array([20, 5])
+
+S1 = Distribution(positions1, masses1)
+S2 = Distribution(positions2, masses2)
+
+W = WassersteinNetwork(S1, [S2], L1Distance(), None)
+W.add_simple_trash(5)
 W.build()
 W.set_point([1.0])
 print("Total cost:", W.total_cost())
 print(W.subgraphs())
 print(W.subgraphs()[0].as_netowkrx())
+W.subgraphs()[0].show()
