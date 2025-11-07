725da2d6f92ae75b06865f792fe26130b8c6ad88
Michał Startek <michal.startek@mimuw.edu.pl>
2025-11-07 15:39:10 +0100

    Some more testing

---
+++ tests/common_testing.py
@@ -0,0 +1,34 @@
+import numpy as np
+from wnet import Distribution, WassersteinNetwork
+from wnet.distances import L1Distance
+
+def create_large_distribution(seed, dimension, pos_range, int_range, size):
+    rng = np.random.default_rng(seed)
+    data = rng.uniform(size=(dimension, size)) * pos_range
+    intensities = rng.uniform(size=(size,)) * int_range
+    return Distribution(data, intensities)
+
+def create_large_wsdflow_instance(seed, dimension, pos_range, int_range, size, no_theoretical_distributions, point):
+    experimental_distribution = create_large_distribution(seed, dimension, pos_range, int_range, size)
+    theoretical_distributions = [
+        create_large_distribution(seed + i + 1, dimension, pos_range, int_range, size)
+        for i in range(no_theoretical_distributions)
+    ]
+    wsdflow_instance = WassersteinNetwork(
+        experimental_distribution,
+        theoretical_distributions,
+        distance=L1Distance(),
+        max_distance=int(pos_range * dimension / 10),
+    )
+    wsdflow_instance.build()
+    return (wsdflow_instance, point)
+
+def solve_large_wsdflow_instance(wsdflow_instance_point):
+    wsdflow_instance, point = wsdflow_instance_point
+    if point is not None:
+        wsdflow_instance.solve(point)
+    else:
+        wsdflow_instance.solve()
+    return wsdflow_instance.total_cost()
+
+
--- tests/test_large.py
+++ tests/test_large.py
@@ -1,35 +1,5 @@
+from common_testing import create_large_wsdflow_instance, solve_large_wsdflow_instance
 import numpy as np
-from wnet import Distribution, WassersteinNetwork
-from wnet.distances import L1Distance
-
-def create_large_distribution(seed, dimension, pos_range, int_range, size):
-    rng = np.random.default_rng(seed)
-    data = rng.uniform(size=(dimension, size)) * pos_range
-    intensities = rng.uniform(size=(size,)) * int_range
-    return Distribution(data, intensities)
-
-def create_large_wsdflow_instance(seed, dimension, pos_range, int_range, size, no_theoretical_distributions, point):
-    experimental_distribution = create_large_distribution(seed, dimension, pos_range, int_range, size)
-    theoretical_distributions = [
-        create_large_distribution(seed + i + 1, dimension, pos_range, int_range, size)
-        for i in range(no_theoretical_distributions)
-    ]
-    wsdflow_instance = WassersteinNetwork(
-        experimental_distribution,
-        theoretical_distributions,
-        distance=L1Distance(),
-        max_distance=int(pos_range * dimension / 10),
-    )
-    wsdflow_instance.build()
-    return (wsdflow_instance, point)
-
-def solve_large_wsdflow_instance(wsdflow_instance_point):
-    wsdflow_instance, point = wsdflow_instance_point
-    if point is not None:
-        wsdflow_instance.solve(point)
-    else:
-        wsdflow_instance.solve()
-    return wsdflow_instance.total_cost()
 
 parameter_set = [
     {
@@ -95,7 +65,8 @@ try:
     def test_large_wsdflow_instances(params, expected):
         wsd_instance = create_large_wsdflow_instance(**params)
         cost = solve_large_wsdflow_instance(wsd_instance)
-        assert np.isclose(cost, expected)
+        print(f"Computed cost: {cost}, Expected cost: {expected}")
+        assert cost == expected
 except ImportError:
     pass
 
