6402bda524ac2b124d25e79af21b49b0ff81cd01
Michał Startek <michal.startek@mimuw.edu.pl>
2025-11-11 18:33:12 +0100

    Example c++ app

---
+++ src/wnet/cpp/wnet/example.cpp
@@ -0,0 +1,43 @@
+#include "distribution.hpp"
+#include "decompositable_graph.hpp"
+#include "graph_elements.hpp"
+
+
+int main()
+{
+    auto rng = std::mt19937(42);
+    VectorDistribution<2> dist = VectorDistribution<2>::CreateRandom(
+        10000,
+        100.0,
+        10,
+        rng
+    );
+    VectorDistribution<2> dist2 = VectorDistribution<2>::CreateRandom(
+        10000,
+        100.0,
+        10,
+        rng
+    );
+    VectorDistribution<2> dist3 = VectorDistribution<2>::CreateRandom(
+        10000,
+        100.0,
+        10,
+        rng
+    );
+
+    std::function<LEMON_INT(const VectorDistribution<2>::Point_t&,
+                            const VectorDistribution<2>::Point_t&)> dist_func =
+        [](const VectorDistribution<2>::Point_t& p1,
+           const VectorDistribution<2>::Point_t& p2) -> VectorDistribution<2>::intensity_type {
+            return static_cast<VectorDistribution<2>::intensity_type>(l1_distance<2, double>(p1, p2));
+        };
+
+    WassersteinNetwork<LEMON_INT, VectorDistribution<2>> wnet(
+        &dist,
+        {&dist2, &dist3},
+        dist_func,
+        10
+    );
+
+    // wnet.solve
+}
