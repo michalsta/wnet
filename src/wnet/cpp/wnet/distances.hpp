f9a6a3ff09a2a54a6bf5e4c2a5414ffc0d0ebca3
Michał Startek <michal.startek@mimuw.edu.pl>
2025-11-21 21:21:16 +0100

    Impl linf norm

--- src/wnet/cpp/wnet/distances.hpp
+++ src/wnet/cpp/wnet/distances.hpp
@@ -39,4 +39,21 @@ inline double l2_distance(
     }
 }
 
+template<size_t DIM, typename position_type = double>
+inline double linf_distance(
+    const std::array<position_type, DIM>& p1,
+    const std::array<position_type, DIM>& p2
+) {
+    if constexpr (DIM == 0) {
+        return 0.0;
+    } else if constexpr (DIM == 1) {
+        return std::abs(p1[0] - p2[0]);
+    } else {
+        // Use fold expression to unroll the loop at compile time
+        return [&]<size_t... Is>(std::index_sequence<Is...>) {
+            return std::max({std::abs(p1[Is] - p2[Is])...});
+        }(std::make_index_sequence<DIM>{});
+    }
+}
+
 #endif // WNET_DISTANCES_HPP
