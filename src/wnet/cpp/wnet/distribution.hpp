9047e15b9d27615db50caa03997dbc3a308e3f1d
Michał Startek <michal.startek@mimuw.edu.pl>
2026-03-09 22:19:30 +0100

    Remove std::iota for windows

--- src/wnet/cpp/wnet/distribution.hpp
+++ src/wnet/cpp/wnet/distribution.hpp
@@ -92,8 +92,10 @@ public:
         if (positions.size() != intensities_vector.size()) {
             throw std::invalid_argument("Positions and intensities must have the same size");
         }
-        sorted_indices.resize(positions.size());
-        std::iota(sorted_indices.begin(), sorted_indices.end(), 0);
+        sorted_indices.reserve(positions.size());
+        for(size_t i = 0; i < positions.size(); ++i) {
+            sorted_indices.push_back(i);
+        }
         std::sort(sorted_indices.begin(), sorted_indices.end(),
                   [this](size_t i1, size_t i2) {
                       return positions[i1][0] < positions[i2][0];
